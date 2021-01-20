198d91b6edf37bdfcdabd5f39b72fc2d49bc194a
lamineMehadji <mehadji.lm@gmail.com>
2021-01-20 14:09:13 +0100

    Les tableaux

---
+++ tableaux/tableaux.c
@@ -0,0 +1,65 @@
+#include "tableaux.h"
+
+int sommeTableau(int* tableau)
+{
+	int	somme;
+	int	i;
+
+	somme = 0;
+	i = -1;
+	while (++i < sizeof(*tableau))
+		somme = somme + *(tableau + i);
+	return (somme);
+}
+
+double moyenneTableau(int* tableau)
+{
+	double	moyenne;
+	int	i;
+
+	moyenne = 0;
+	i = -1;
+	while (++i < sizeof(*tableau))
+		moyenne = (double)sommeTableau(tableau) / (double)sizeof(*tableau);
+	return (moyenne);
+}
+
+void copieTableau(int* tableauOriginal, int* tableauCopie)
+{
+	int	i;
+
+	i = -1;
+	while (++i < sizeof(*tableauOriginal))
+		*(tableauCopie + i) = *(tableauOriginal + i);
+}
+
+void maximumTableau(int* tableau, int valeurMax)
+{
+	int	i;
+
+	i = -1;
+	while (++i < sizeof(*tableau))
+		if (*(tableau + i) > valeurMax)
+			*(tableau + i) = 0;
+}
+
+void ordonnerTableau(int* tableau)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = -1;
+	while (++i < sizeof(*tableau))
+	{
+		j = i;
+		while (++j < sizeof(*tableau))
+			if (*(tableau + i) > *(tableau + j))
+			{
+				temp = *(tableau + i);
+				*(tableau + i) = *(tableau + j);
+				*(tableau + j) = temp;
+				j = i;
+			}
+	}
+}
---
+++ tableaux/tableaux.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int		sommeTableau(int*);
+double	moyenneTableau(int*);
+void	copieTableau(int*, int*);
+void	maximumTableau(int*, int);
+void	ordonnerTableau(int*);
