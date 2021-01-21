2097fca62db7ecb3d842231ca47604b168d503c7
lamineMehadji <mehadji.lm@gmail.com>
2021-01-21 19:32:59 +0100

    define avec macro sans parametres

---
+++ helloWorld/helloWorld.c
@@ -0,0 +1,7 @@
+#include "helloWorld.h"
+
+int main(int argc, char **argv)
+{
+	HELLOWORLD()
+	return 0;
+}
---
+++ helloWorld/helloWorld.h
@@ -0,0 +1,8 @@
+#ifndef DEF_HELLOWORLD
+#define DEF_HELLOWORLD
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+ #define HELLOWORLD() printf("Hello world!\n");
+
+#endif
